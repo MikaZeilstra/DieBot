0c5d7d83c3a19ebd32bd1fe9b8cf2313bb34653a
MikaZeilstra <mika.zeilstra@gmail.com>
2019-02-03 19:55:59 +0100

    Removed ping functionality. Added Basic bot framework. Added Help page.

--- bot.js
+++ bot.js
@@ -2,15 +2,34 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token =  require('token');
 
+
 client.on("ready" , function(){
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
 client.on('message', function(msg) {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
+    //check if the message is send in the dnd channel and a command for the bot
+    if(msg.channel.id === "538714873132220419"){
+        if(msg.content.substr(0,1) === "\\"){
+            /*************
+            Check command
+            *************/
+
+            //help command
+            if(msg.content.substr(1,4) === "help"){
+                msg.reply("\nHello I'am dice bot these commands are currently available:\n" +
+                          "```\\help : This page!\n" +
+                          "\\die [Type] [Amount]: Rolls [Amount] die of type [Type]. ```"
+                )
+            }
+
+            //die commands
+            if(msg.content.substr(1,3) === "die"){
+                msg.reply("")
+            }
+
+        }
     }
 });
 
-console.log((new token).getToken());
 client.login((new token).getToken());
