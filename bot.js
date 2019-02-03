12b2f849583c05e238f3fc1e74b0d0fe73e8ac79
MikaZeilstra <mika.zeilstra@gmail.com>
2019-02-03 18:42:37 +0100

    fixed ping functionality;

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = new require('token');
+const token =  require('token');
 
 client.on("ready" , function(){
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
-client.on('message', function() {
+client.on('message', function(msg) {
     if (msg.content === 'ping') {
       msg.reply('Pong!');
     }
 });
 
-client.login(token.getToken());
+console.log((new token).getToken());
+client.login((new token).getToken());
