c8d6f7ad5cff2dedae9251f2a70274461d885f9c
MikaZeilstra <mika.zeilstra@gmail.com>
2019-02-03 21:12:15 +0100

    Move Apikey try #3

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 
 client.on("ready" , function(){
     console.log(`Logged in as ${client.user.tag}!`);
