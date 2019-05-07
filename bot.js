b9eebabd2fb78ba649eade142a556ebc478b18cd
Mika Zeilstra <M.J.Zeilstra@student.tudelft.nl>
2019-05-07 14:06:53 +0200

    Removed only DND channel constraint

--- bot.js
+++ bot.js
@@ -9,39 +9,35 @@ client.on("ready" , function(){
 })
 
 client.on('message', function(msg) {
-    //check if the message is send in the dnd channel and a command for the bot
-    if(msg.channel.id === "538714873132220419"){
         if(msg.content.substr(0,1) === "\\"){
-            /*************
-            Check command
-            *************/
+        /*************
+        Check command
+        *************/
 
-            //help command
-            if(msg.content.substr(1,4) === "help"){
-                msg.reply("\nHello I'am die bot these commands are currently available:\n" +
-                          "```\\help : This page!\n" +
-                          "\\roll [Amount] [Type]: Rolls [Amount] die of type [Type] eg \\roll 2 d20. ```"
-                )
+        //help command
+        if(msg.content.substr(1,4) === "help"){
+            msg.reply("\nHello I'am die bot these commands are currently available:\n" +
+                      "```\\help : This page!\n" +
+                      "\\roll [Amount] [Type]: Rolls [Amount] die of type [Type] eg \\roll 2 d20. ```"
+            )
+        }
+
+        //die commands
+        if(msg.content.substr(1,4) === "roll"){
+            try {
+                var Amount = parseInt(msg.content.split(" ")[1]);
+                var Type = parseInt(msg.content.split(" ")[2].substr(1));
+            } catch (error) {
+                msg.reply("Uh-oh Wrong arguments this command is used like this:\n"+ 
+                "```\\roll [Amount] [Type]: Rolls [Amount] die of type [Type] eg \\roll 2 d20. ```");
+                return;
             }
-
-            //die commands
-            if(msg.content.substr(1,4) === "roll"){
-                try {
-                    var Amount = parseInt(msg.content.split(" ")[1]);
-                    var Type = parseInt(msg.content.split(" ")[2].substr(1));
-                } catch (error) {
-                    msg.reply("Uh-oh Wrong arguments this command is used like this:\n"+ 
-                    "```\\roll [Amount] [Type]: Rolls [Amount] die of type [Type] eg \\roll 2 d20. ```");
-                    return;
-                }
                 
-                var result = "Results:\n";
-                for(var i  = 0;i < Amount; i++){
-                    result += (i+1) + " :" + (Math.floor(Math.random()*Type) + 1) + "\n"
-                }
-                msg.reply("\n" + result);
+            var result = "Results:\n";
+            for(var i  = 0;i < Amount; i++){
+                result += (i+1) + " :" + (Math.floor(Math.random()*Type) + 1) + "\n"
             }
-
+            msg.reply("\n" + result);
         }
     }
 });
